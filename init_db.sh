6991fc4babf697a577d1c6a99e387c7e2a452077
zrs <zrs@rowenatech.com>
2019-07-05 22:02:41 +0800

    add config

--- init_db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-python manage.py makemigrations
-python manage.py migrate
-
-
-#python manage.py loaddata author_detail.json   # 先author_detail，再author（两者有一对一关联）
-#python manage.py loaddata author.json
--- prod_entrypoint.sh
+++ prod_entrypoint.sh
@@ -4,5 +4,24 @@ cd /work
 
 echo "starting ..."
 #ls
-pwd
+#pwd
+
+# 收集django框架静态文件到STATIC_ROOT目录
+python manage.py collectstatic
+
+if ["$DEBUG" == "true"]; then
+    echo "use debug mode"
+    echo "load data to db ... "
+    # 生成表
+    python manage.py makemigrations
+    # 迁移表
+    python manage.py migrate
+
+    # 加载初始数据到db
+    #python manage.py loaddata author_detail.json   # 先author_detail，再author（两者有一对一关联）
+    #python manage.py loaddata author.json
+    echo "load done!"
+fi
+
+
 exec "$@"
