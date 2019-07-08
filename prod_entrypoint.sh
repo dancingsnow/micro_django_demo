a15512f1df843e193808227ad0b70f3facb063f7
zrs <zrs@rowenatech.com>
2019-07-08 15:33:09 +0800

    添加数据库依赖镜像

--- prod_entrypoint.sh
+++ prod_entrypoint.sh
@@ -3,23 +3,22 @@
 cd /work
 
 echo "starting ..."
-#ls
-#pwd
 
 # 收集django框架静态文件到STATIC_ROOT目录
 python manage.py collectstatic
 
-if ["$DEBUG" == "true"]; then
-    echo "use debug mode"
-    echo "load data to db ... "
+# 初始化数据库
+if ["$INIT_DB" == "true"]; then
+    echo "Initialize DB table ... "
     # 生成表
     python manage.py makemigrations
     # 迁移表
     python manage.py migrate
 
     # 加载初始数据到db
-    #python manage.py loaddata author_detail.json   # 先author_detail，再author（两者有一对一关联）
-    #python manage.py loaddata author.json
+    echo "load data to DB table ... "
+    python manage.py loaddata author_detail.json   # 先author_detail，再author（两者有一对一关联）
+    python manage.py loaddata author.json
     echo "load done!"
 fi
 
